4bd8d56104a362bf58f3d4ab3190e41d428ed670
Nathanael L. Baisa <nathanaellmss@gmail.com>
2021-09-25 21:07:47 +0100

    Update recursive_algorithms.h

--- include/recursive_algorithms.h
+++ include/recursive_algorithms.h
@@ -1,1 +1,11 @@
+#include <pybind11/pybind11.h>  // pybind11 — Seamless operability between C++11 and Python.
 
+unsigned long fibonacci(const unsigned int n);
+unsigned long factorial(const unsigned int n);
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(pybind_11_example, mod) {
+    mod.def("fibonacci_cpp", &fibonacci, "Recursive fibonacci algorithm.");
+    mod.def("factorial_cpp", &factorial, "Recursive factorial algorithm.");
+}
