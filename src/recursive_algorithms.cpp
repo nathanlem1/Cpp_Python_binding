7d1cfeeba07cbab943e183f99a939a1a6951a6fe
Nathanael L. Baisa <nathanaellmss@gmail.com>
2021-09-26 13:34:55 +0100

    Update recursive_algorithms.cpp

--- src/recursive_algorithms.cpp
+++ src/recursive_algorithms.cpp
@@ -5,7 +5,9 @@ unsigned long fibonacci(const unsigned int n){
     if (n < 2){
         return n;
     }
-    return fibonacci(n - 1) + fibonacci(n - 2);
+    else{
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 }
 
 
@@ -14,6 +16,7 @@ unsigned long factorial(const unsigned int n){
     if (n == 0 || n == 1){
         return 1;
     }
-    return n * factorial (n - 1);
+    else{
+        return n * factorial (n - 1);
+    }
 }
-
