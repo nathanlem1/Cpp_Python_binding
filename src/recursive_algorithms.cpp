26ea9607d92c02927816a3c87f96c91656338399
Nathanael L. Baisa <nathanaellmss@gmail.com>
2021-09-26 23:18:01 +0100

    Update recursive_algorithms.cpp

--- src/recursive_algorithms.cpp
+++ src/recursive_algorithms.cpp
@@ -1,8 +1,12 @@
+#include <iostream>
 #include "recursive_algorithms.h"
 
-unsigned long fibonacci(const unsigned int n){
+unsigned long int fibonacci(const unsigned int n){
 
-    if (n < 2){
+    if (n < 0){
+        std::cout << "Error!, fibonacci does not exist for negative numbers." << std::endl;
+    }
+    else if (n < 2){
         return n;
     }
     else{
@@ -10,10 +14,12 @@ unsigned long fibonacci(const unsigned int n){
     }
 }
 
+unsigned long long int factorial(const unsigned int n){
 
-unsigned long factorial(const unsigned int n){
-
-    if (n == 0 || n == 1){
+    if (n < 0){
+        std::cout << "Error!, factorial does not exist for negative numbers." << std::endl;
+    }
+    else if (n == 0 || n == 1){
         return 1;
     }
     else{
